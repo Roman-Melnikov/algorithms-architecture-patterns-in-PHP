9b4e4d9ca288f982785a76ad50ed76278ee366b8
Roman <Melnikov_RV@list.ru>
2022-09-23 14:05:48 +0700

    dz-7

---
+++ task-1.php
@@ -0,0 +1,12 @@
+Задание:
+Найти и указать в проекте Front Controller и расписать классы, которые с ним
+взаимодействуют.
+
+
+Kernel является в данном проекте Front Controller.
+
+Взаимодействует с классами:
+    - MainController;
+    - OrderController;
+    - ProductController;
+    - UserController.
